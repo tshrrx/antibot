6fb6c537e7331b2dee890b0710a1d7e4fc77aa3f
Devansh <devanshpratap1500@gmail.com>
2024-10-26 15:19:00 +0530

    particle effect fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ParticleBackground from '../ParticleBackground'
-import './css/home.css'
+import React from "react";
+import ParticleBackground from "../ParticleBackground";
+import "./css/home.css";
 
 const Home = () => {
   return (
     <div className="App">
-            <ParticleBackground />
-            <div className="content">
-                <h1>Particle Background</h1>
-                <p>TEXT</p>
-            </div>
-        </div>
-  )
-}
+      <ParticleBackground />
+      <div className="content">
+        <h1>Particle Background</h1>
+        <p>TEXT</p>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
