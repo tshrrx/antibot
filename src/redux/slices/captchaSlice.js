c94b93eae06b1f6aa2a1bd0a95fbbef57209e222
Devansh <devanshpratap1500@gmail.com>
2024-10-29 22:38:54 +0530

    mouse movement trace operation completed

--- src/redux/slices/captchaSlice.js
+++ src/redux/slices/captchaSlice.js
@@ -1,18 +1,26 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    flag:false,
+    mouseMovements: [], // Updated state property
+    flag: false, // Ensure flag is initialized
+    clicks: [], // New state property
 };
 
 const captchaSlice = createSlice({
     name: 'captcha',
     initialState,
     reducers: {
-        setFlag(state,value){
-            state.flag=value.payload;
+        setFlag(state, action) {
+            state.flag = action.payload;
         },
+        addMouseMovement(state, action) {
+            state.mouseMovements.push(action.payload);
+        },
+        addClick(state, action) {
+            state.clicks.push(action.payload);
+        }
     },
 });
 
-export const {setFlag} = captchaSlice.actions;
+export const { setFlag, addMouseMovement,addClick } = captchaSlice.actions;
 export default captchaSlice.reducer;
