7b3a2eed4b2cb924300c32730f1ca1e9de00f96c
tshrrx <144604940+tshrrx@users.noreply.github.com>
2024-11-02 13:49:15 +0530

    landingpage_update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,62 @@ const Home = () => {
   return (
     <div className="App">
       <ParticleBackground />
-      <div className="content">
-        <h1>AntiBot</h1>
-        <p></p>
+      <div className="header">
+        <h2></h2>
+        <div className="mid-spot"></div>
+        <button className="github" onClick={() => window.open('https://github.com/Devansh1508/antibot', '_blank', 'noopener,noreferrer')}>
+          <span className="glow"></span>
+          <span className="github-content">GitHub</span>
+        </button>
+        <div className="spotlight">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
+
+      <div className="accent-lines">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div className="heroSubP">
+        <p>Introducing</p>
+      </div>
+      <div className="hero">
+        <div className="heroT">
+          <h2>ANTIBOT</h2>
+          <h2>ANTIBOT</h2>
+        </div>
+      </div>
+      <p className="heroP">CAPTCHA Security<br></br>with Intelligent Mouse Dynamics</p>
+      <div className="mountains">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="hero-spacer"></div>
+
+      {/* <div className="content-section">
+        <div className="content-acc">
+          <div></div>
+          <div></div>
+        </div>
+        <p className="subt">Revolutionary by design</p>
+        <h3 className="title">Harness. Empower.<br></br>
+          Unmatched Versatility.</h3>
+        <p className="subp">At the core lies our revolutionary framework, <br></br>ensuring adaptability across all application architectures.</p>
+      </div> */}
     </div>
   );
 };
