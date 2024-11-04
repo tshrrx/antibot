807b11c62c6c1636e0d8c98af02989940d968053
Devansh <devanshpratap1500@gmail.com>
2024-11-04 22:24:26 +0530

    login page completed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -44,21 +44,21 @@ const LoginPage = () => {
 
 
   const styling =
-    "m-1 border-2 flex justify-center items-center p-[1px] rounded-xl";
+    "m-1 w-[17vw] border-2 flex justify-center items-center p-[1px] rounded-xl";
 
   return (
     <div className="Page">
         <ParticleBackground />
         {(flag)?
         <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-            <div>
-              <div className="my-[10vh]">
+            <div className="border-2  rounded-xl border-[#D1D5DB] p-20 bg-[#d1d5db28]">
+            <div className="mb-[10vh]">
                 <Header content={"Login"}/>
               </div>
               
               <form
               action=""
-              className="loginForm flex flex-col max-[800px]:text-[12px]"
+              className="loginForm flex justify-center items-center gap-2 flex-col max-[800px]:text-[12px]"
               onSubmit={handleSubmit(onSubmit)}
             >
               <input
@@ -76,7 +76,7 @@ const LoginPage = () => {
               {
                 !nav&&(
                   <input
-                className="submitBtn text-center text-white textShado transition-all duration-200 hover:scale-95 px-6 py-2 shadow-xl rounded-md onhover:scale-95 font-bold m-3 bg-[#1d1d1d]"
+                className="submitBtn w-[10vw] mt-11 text-center text-white textShado transition-all duration-200 hover:scale-95 px-6 py-2 shadow-xl rounded-md onhover:scale-95 font-bold m-3 bg-[#1d1d1d]"
                 disabled={isSubmitting}
                 type="Submit"
                 name="Submit"
